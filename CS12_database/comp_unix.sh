024a37dc42a50a6c9bcdeb8d1bd0a4dc1a121a89
= <tkf.x1os@gmail.com>
2025-08-22 10:50:23 +1200

    Fri Aug 22 10:50:23 AM NZST 2025

--- CS12_database/comp_unix.sh
+++ CS12_database/comp_unix.sh
@@ -3,6 +3,12 @@ git add .
 git commit -m "$(date)"
 
 echo ""
-gcc -g main.c lib/*.c -o database && ./database $@  
+gcc -g main.c lib/*.c -o database  
+
+#show options 
+./database 
+
+# run options 
+./database $@  
 echo ""
 git push
