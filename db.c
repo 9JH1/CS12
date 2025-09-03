d86003b177e17080e5902ce2cb1f79a99c1bceb8
= <tkf.x1os@gmail.com>
2025-09-04 11:08:30 +1200

    Thu Sep  4 11:08:30 AM NZST 2025

--- db.c
+++ db.c
@@ -759,7 +759,7 @@ int database() {
       ret = ui_menu(
           (const char **)member_menu, db_members_index,
           "View Members (showing last names and sorted by time_created)\n");
-      print_member_data(db_members[ret]);
+      print_member_data(db_members[ret],ret);
 
     } else {
       ret = ui_menu((const char **)book_menu, db_books_index, "View Books\n");
