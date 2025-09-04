68ac228b938da77ac3f776f854c80227be48cbab
= <tkf.x1os@gmail.com>
2025-09-04 16:40:26 +1200

    Thu Sep  4 04:40:26 PM NZST 2025

--- db.c
+++ db.c
@@ -811,7 +811,7 @@ int database() {
       }
     }
 
-		printf("test");
+		printf("test %d\n",db_books_index);
 
     for (int i = 0; i < index; i++) {
       printf("%s: %d\n", genres[i].genre, genres[i].count);
