9979bcc18255fbbc68a70b2c6d133e294ca68668
= <tkf.x1os@gmail.com>
2025-10-07 15:26:22 +1300

    added a build to src

--- CS12_database/comp.sh
+++ CS12_database/comp.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
+
+rm -rf src/
+
 if [ ! -d "src" ];then 
 	mkdir src 
 fi 
 read -p "Enter database flags: " db_flags
+extra_flags="# -v -g"
 
-if [[ "$1" = "_WIN32" ]] || [[ "$1" = "_MINGW32" ]];then
-	x86_64-w64-mingw32-gcc -g main.c lib/*.c -g -o src/database -O2 # -v
+if [[ "$1" = "FIXWIN" ]]; then
+	icacls "." /reset /T
+	takeown /f "." /r 
+elif [[ "$1" = "_WIN32" ]] || [[ "$1" = "_MINGW32" ]];then
+	x86_64-w64-mingw32-gcc main.c lib/*.c -o src/database $extra_flags 
+	if [ $? -ne 0 ]; then 
+		echo "$1 compilation failed"
+		exit 1
+	fi 
+
 	if [[ "$1" = "_MINGW32" ]];then 
 		wine ./src/database.exe $db_flags 
 	else 
 		./src/database.exe $db_flags 
 	fi
-else  
 
+else 
 	if [[ !"$1" = "_NOGIT" ]];then 
 		git add . &>/dev/null 
 		git commit -m "$(date)" &>/dev/null 
 	fi 
 
-	gcc -g main.c lib/*.c -g -o src/database -O2 # -v
+	gcc -g main.c lib/*.c -o src/database $extra_flags
+	if [ $? -ne 0 ]; then
+		echo "$1 compilation failed"
+		exit 1
+	fi 
 	./src/database $db_flags
 
 	if [[ ! "$1" = "_NOGIT" ]];then 
 		git push &>/dev/null
 	fi
-	echo "Done!"
 fi
+
+echo "$1 finished"
