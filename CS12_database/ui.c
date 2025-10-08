30199989faef216d543f77ad66af4baecad68661
9jh1 <tkf.x1os@gmail.com>
2025-10-08 21:44:45 +1300

    Wed Oct  8 09:44:45 PM NZDT 2025

--- CS12_database/ui.c
+++ CS12_database/ui.c
@@ -148,7 +148,7 @@ void ui_print(const char *in) {
   size_t len = strlen(in); // Cache the string length
 
   for (size_t i = 0; i < len; i++) {
-		usleep(10000);
+		usleep(2000);
 		fflush(stdout);
     const char cur = in[i];
 
