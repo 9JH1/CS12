7fd9d21162899016bef95491cc9722e2458c4979
= <tkf.x1os@gmail.com>
2025-10-09 10:40:20 +1300

    Thu Oct  9 10:40:20 AM NZDT 2025

--- db_new/build/comp.sh
+++ db_new/build/comp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 build_path="build/src/"
-extra_flags="-Wall -Wextra"
+extra_flags="-Wall -Wextra -Wno-unused-function"
 file_name="db"
 main_flags="main.c src/*.c src/remote/plib/*.c -o $build_path$file_name $extra_flags"
 
