8e18d89de32f1c31270a4661ff5ea299bccdb8a7
= <tkf.x1os@gmail.com>
2025-10-09 12:48:10 +1300

    Thu Oct  9 12:48:10 PM NZDT 2025

--- CS12_database/ui.c
+++ CS12_database/ui.c
@@ -11,8 +11,8 @@
 #include <string.h>
 #include <unistd.h>
 
-#define COL_SIZE_2 60
-#define COL_SIZE 50
+#define COL_SIZE_2 65
+#define COL_SIZE 55
 void ui_quit(int code) {
   show_cursor();
   exit(code);
