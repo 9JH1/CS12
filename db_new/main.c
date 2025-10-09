391ca9cc47f1c48e6a4f7abc588799127d29ec67
9jh1 <tkf.x1os@gmail.com>
2025-10-09 18:07:03 +1300

    Thu Oct  9 06:07:03 PM NZDT 2025

--- db_new/main.c
+++ db_new/main.c
@@ -1,37 +1,41 @@
-/** 
- * @file main.c 
- * @brief Handles main scripting. 
+/**
+ * @file main.c
+ * @brief Handles main scripting.
  **/
 #include "include.h"
 #include "src/remote/plib/plib.h"
 
 /**
  * @brief main database loop
- * @param c the count of system arguments 
+ * @param c the count of system arguments
  * @param v an array of system arguments
  *
- * this function is the first bit of 
- * code run in the database, it handles 
+ * This function is the first bit of
+ * code run in the database, it handles
  * all of the argument declarations,
- * initialization and many other functions 
+ * initialization and many other functions
  **/
-int main(const int c, const char *v[]){
-	pl_arg *db_help = PL_P("--help","show this dialog");
-	pl_arg *db_init = PL_P("--init", "initialize the database");
-	pl_arg *db_delete = PL_P("--clean","remove the database");
-	pl_arg *db_no_write = PL_P("--nowrite","remove write privleges from current user");
-	pl_r ret;
+int main(const int c, const char *v[]) {
+  pl_arg *db_help = PL_P("--help", "show this dialog");
+  pl_arg *db_init = PL_P("--init", "initialize the database");
+  pl_arg *db_delete = PL_P("--clean", "remove the database");
+  pl_arg *db_no_write = PL_P("--nowrite", "disable write");
+  pl_r ret;
 
-	if((ret = pl_proc(c,v)) == PL_SUCCESS){
-		printf("Hello World!\n");
-	} else { // handle errors
-		help();
-		printf("error: '%s' from argument '%s'\n",
-				PL_E(ret), // error code stringify'd
-				PL_LAST_ARG); // the last arg parsed in plib 
-		quit(HELP_MENU_REF);
+  if ((ret = pl_proc(c, v)) == PL_SUCCESS) {
+    printf("Hello World!\n");
+		
+		if(PL_R(db_help)) goto help_exit;
 	}
 
-	quit(SUCCESS);
-}
+	// help dialog code
+	help_exit:
+	help();
 
+	// print error details and exit
+  printf("error: '%s' from argument '%s'\n",
+         PL_E(ret),    // error code stringify'd
+         PL_LAST_ARG); // the last arg parsed by plib
+
+	quit(HELP_MENU_REF);
+}
