1adc4d606cccae13959bd65a6b6ccf2fc0478acf
= <tkf.x1os@gmail.com>
2025-10-09 14:31:03 +1300

    Thu Oct  9 02:31:03 PM NZDT 2025

--- CS12_database/ui.c
+++ CS12_database/ui.c
@@ -81,7 +81,7 @@ int ui_menu(const char *array1[], const int size, const char *array2[],
     printf("%s\033[0m\n", bor_s.string);
     gotoxy(0, 0);
 
-    printf("%s (%d total lines) (current datetime: %d/%d/%d %d:%d:%d)\n",
+    printf("%s (%d total lines) (current datetime: %d/%d/%d %d:%d:%d) press [f] to filter\n",
            prompt, size, rn.day, rn.month, rn.year, rn.hour, rn.minute,
            rn.second);
 
